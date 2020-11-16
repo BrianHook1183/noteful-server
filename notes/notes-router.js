7c52c94b105739e4da54297337258135ad78ffe9
Brian Hook <34469882+BrianHook1183@users.noreply.github.com>
2020-11-16 02:55:08 -0600

    routers created for GET

---
+++ notes/notes-router.js
@@ -0,0 +1,28 @@
+const path = require('path');
+const express = require('express');
+const xss = require('xss');
+const NotesService = require('./notes-service');
+
+const notesRouter = express.Router();
+const jsonParser = express.json();
+
+const serializeNote = note => ({
+  id: note.id,
+  name: xss(note.name),
+  modified: note.modified,
+  content: xss(note.content),
+  folder_id: note.folder_id
+});
+
+notesRouter
+  .route('/')
+  .get((req, res, next) => {
+    const knexInstance = req.app.get('db');
+    NotesService.getAllNotes(knexInstance)
+      .then(notes => {
+        res.json(notes.map(serializeNote));
+      })
+      .catch(next);
+  });
+
+module.exports = notesRouter;
